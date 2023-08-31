c546b9c730c6e3410f204717e4343a33fcf26759
Yana <yanachernushevich@gmail.com>
2023-08-31 22:27:23 +0100

    refactored

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,20 +1,8 @@
-import { TitleDescription } from './TitleDescription/title-description.component'
 import { Button } from './Button/button.component'
 import { Title } from './Title/title.component'
+import { Paragraph } from './Paragraph/paragraph.component'
 import { SubTitle } from './SubTitle/subtitle.component'
-import { SubTitleTop } from './SubTitleTop/subtitle-top.component'
 import { Card } from './Card/card.component'
 import { Scroll } from './Scroll/scroll.component'
-import { TitleSmall } from './TitleSmall/title-small.component'
 
-export {
-  TitleDescription,
-  Button,
-  Title,
-  SubTitle,
-  SubTitleTop,
-  Card, 
-  Scroll, 
-  TitleSmall
-
-}
+export { Button, Title, Paragraph, SubTitle, Card, Scroll }
