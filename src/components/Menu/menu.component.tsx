b562fbbde3f9c9187a050eddd8fd73a1c70ff321
Yana <yanachernushevich@gmail.com>
2023-10-04 14:56:34 +0100

    changed menu styles

--- src/components/Menu/menu.component.tsx
+++ src/components/Menu/menu.component.tsx
@@ -8,12 +8,13 @@ export const Menu = ({
 	isMenuOpen: boolean
 	setIsMenuOpen(stat: boolean): void
 }) => {
-	const activClass = isMenuOpen
-		? ` flex flex-col justify-start items-center absolute top-[80px] w-[100%] h-[400px] left-0 bg-backgroundMain z-10 sm:hidden sidebar`
-		: ` hidden sm:flex sm:flex-row align-center`
 
 	return (
-		<nav className={activClass}>
+		<nav
+			className={`sm:flex sm:flex-row sm:items-center sm:pb-0 pb-[70px] absolute sm:static bg-backgroundMain sm:z-auto  w-full sm:w-auto transition-all duration-500 ease-in-out ${
+				isMenuOpen ? 'top-[80px] opacity-100  left-0 ' : 'top-[-400px] left-0 '
+			} sm:opacity-100 opacity-0 bg-backgroundMain`}
+		>
 			{navLinks.map((link: { id: string; title: string }) => (
 				<Link key={link.id} id={link.id} setIsMenuOpen={setIsMenuOpen}>
 					{link.title}
@@ -37,7 +38,7 @@ export const Link = ({
 		<a
 			href={`#${id}`}
 			onClick={() => setIsMenuOpen(false)}
-			className={`${hovers.link.parent} text-center w-[100%]`}
+			className={`${hovers.link.parent} text-center w-[100%] z-100 `}
 		>
 			<p className={`${hovers.link.child} sm:py-1 sm:px-4 py-4 px-3`}>
 				{children}
@@ -55,7 +56,7 @@ const RequestButton = ({
 		<a
 			href='#contacts'
 			onClick={() => setIsMenuOpen(false)}
-			className={`${hovers.button.parent} hover:border-brandColorGreenHover w-[100%] flex justify-center items-center text-center rounded border-2 border-brandColorGreen mt-[40px] sm:mt-0 sm:ml-[20px] md:ml-[40px]  min-w-[150px] text-brandColorGreen `}
+			className={`${hovers.button.parent} hover:border-brandColorGreenHover w-[100%] flex justify-center items-center text-center rounded border-2 border-brandColorGreen mt-[40px] sm:mt-0 sm:ml-[20px] md:ml-[40px]  min-w-[150px] text-brandColorGreen bg-backgroundMain  `}
 		>
 			<p className={`${hovers.button.child} py-2 px-4 sm:py-0 `}>
 				Send request
