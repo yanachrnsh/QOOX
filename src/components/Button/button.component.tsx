93e5da654b52eb56a312004444f7bb573ae9a3cf
Yana <yanachernushevich@gmail.com>
2023-10-05 15:32:41 +0100

    added styles for hover on ButtonLarge

--- src/components/Button/button.component.tsx
+++ src/components/Button/button.component.tsx
@@ -19,7 +19,7 @@ export const Button: FC<IButton> = ({ text, svg, id, styles }) => {
 			<p
 				className={` ${hovers.button.child} flex items-center justify-center gap-2`}
 			>
-				{text} {svg && <img src={svg} alt='arrow' />}
+				{text} {svg && <img src={svg} alt='arrow w-5 h-5' />}
 			</p>
 		</a>
 	)
@@ -27,11 +27,21 @@ export const Button: FC<IButton> = ({ text, svg, id, styles }) => {
 
 export const ButtonLarge = ({ style = '' }) => {
 	return (
-		<Button
-			text='Discover Our Expertise'
-			svg={arrowIcon}
-			id='cases'
-			styles={`${styles.flexCenter} relative py-9 px-9 rounded-xl mt-6 mb-22 w-[100%] sm:self-start sm:p-20 lg:h-[100%] lg:mt-0 lg:mb-0  ${style}`}
-		></Button>
+		<a
+			href='#cases'
+			className={`${hovers.backgroundHoverGreen} flex items-center justify-center min-w-[200px] max-w-[100%] text-lightPrimary py-2 px-6 rounded text-center ${styles.flexCenter} group/large relative py-9 px-9 rounded-xl mt-6 mb-22 w-[100%] sm:self-start sm:p-20 lg:h-[100%] lg:mt-0 lg:mb-0 overflow-hidden  ${style} `}
+		>
+			<p className={`flex items-center justify-center relative `}>
+				<span className='z-20 mr-6 group-hover/large:text-brandColorGreen'>
+					Discover Our Expertise
+				</span>
+				<img
+					src={arrowIcon}
+					alt='arrow'
+					className='absolute -right-4 rounded-full z-20 hover:text-brandColorGreen '
+				></img>
+				<div className=' bg-lightPrimary absolute -right-4 w-8 h-8 rounded-full transition duration-500 ease-in-out group-hover/large:scale-[3500%] z-10'></div>
+			</p>
+		</a>
 	)
 }
