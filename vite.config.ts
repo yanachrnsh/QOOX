965b64ed32b8dc55afc840f35a8edcc8f5c8151b
Yana <yanachernushevich@gmail.com>
2023-10-06 18:54:04 +0100

    upt in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,10 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), tsconfigPaths()]
+	plugins: [react(), tsconfigPaths()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src')
+		}
+	}
 })
