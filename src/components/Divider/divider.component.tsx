08e8cae31138c0f4fbecf356084955d9d15f5c1e
Yana <yanachernushevich@gmail.com>
2023-10-24 15:58:37 +0100

    fixed divider

--- src/components/Divider/divider.component.tsx
+++ src/components/Divider/divider.component.tsx
@@ -1,7 +1,7 @@
 export const Divider = () => {
 	return (
 		<hr
-			className={`mx-4 max-w-[1160px] border-b border-stroke-darker xs:mx-4 md:mx-auto`}
+			className={`mx-4 h-px max-w-[1160px] border-t border-stroke-darker xs:mx-4 md:mx-auto`}
 		></hr>
 	)
 }
